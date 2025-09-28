078444eb75e5b196cb91ac5cd4964fab543c29b8
gws <glennws@pm.me>
2025-09-28 14:07:46 +0200

    look around init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use bevy::prelude::*;
+use std::f32::consts::PI;
+
+use bevy::{input::mouse::AccumulatedMouseMotion, prelude::*};
 
 fn spawn_camera(mut cmd: Commands) {
-    cmd.spawn(Camera3d::default());
+    cmd.spawn((Camera3d::default(), Player));
 }
 
 fn spawn_map(
@@ -27,9 +29,30 @@ fn spawn_map(
     }
 }
 
+const PI2: f32 = PI / 2.0;
+
+fn player_look(
+    mut player: Single<&mut Transform, With<Player>>,
+    mouse_motion: Res<AccumulatedMouseMotion>,
+    time: Res<Time>,
+) {
+    let dt = time.delta_secs();
+    let sensitivity = 0.1;
+    use EulerRot::YXZ;
+    let (mut yaw, mut pitch, _) = player.rotation.to_euler(YXZ);
+    yaw -= mouse_motion.delta.x * dt * sensitivity;
+    pitch -= mouse_motion.delta.y * dt * sensitivity;
+    pitch = pitch.clamp(-PI2, PI2);
+    player.rotation = Quat::from_euler(YXZ, yaw, pitch, 0.0);
+}
+
 fn main() {
     let mut app = App::new();
     app.add_plugins(DefaultPlugins);
     app.add_systems(Startup, (spawn_camera, spawn_map));
+    app.add_systems(Update, player_look);
     app.run();
 }
+
+#[derive(Component)]
+struct Player;
